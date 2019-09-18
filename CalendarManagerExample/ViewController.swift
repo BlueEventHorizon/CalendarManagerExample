dd18dcbbb09721ae25fb07314cf491bf15d6b8c3
k_terada <k_terada@yumemi.co.jp>
2019-09-18 20:57:17 +0900

    initial commit

--- CalendarManagerExample/ViewController.swift
+++ CalendarManagerExample/ViewController.swift
@@ -10,11 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var year: UITextField!
+    @IBOutlet weak var month: UITextField!
+    @IBOutlet weak var day: UITextField!
+    
+    @IBAction func pushed(_ sender: Any) {
+        if let date = Date(dateString: "\(year.text ?? "2019")-\(month.text ?? "01")-\(day.text ?? "21") 00:00:00", dateFormat: FormatterType.std.rawValue){
+            CalendarManager.shared.addEvent(title: "テスト登録", start: date)
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        CalendarManager.shared.authorize { _ in
+            _ = CalendarManager.shared.getEvents(from: CalendarManager.shared.getCalendars())
+        }
     }
-
-
 }
 
