ed9d5ad15272088ee3c0c6a2a85c46017a56a528
k_terada <k_terada@yumemi.co.jp>
2019-09-18 20:09:52 +0900

    Initial Commit

---
+++ CalendarManagerExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CalendarManagerExample
+//
+//  Created by 寺田 克彦 on 2019/09/18.
+//  Copyright © 2019 moons. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
